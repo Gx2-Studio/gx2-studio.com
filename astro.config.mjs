d13fe56a06dbd76d59fa3bdfcf320d6de7a9ebaa
gmsec <guillaume.monarque@gmail.com>
2023-06-12 03:53:12 +0000

    Sanglant

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://gx2-studio.com',
+  integrations: [tailwind(), image()]
 });
