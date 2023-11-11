2a12fd738bf47c011e1fa6298a1dca426840965d
gmsec <guillaume.monarque@gmail.com>
2023-11-11 01:32:00 +0000

    Unclutter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gx2-studio.com',
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), compress(), sitemap(), react()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#000000",
+        primary: "#fafafa",
+        pal1: "#264653",
+        pal2: "#2a9d8f",
+        pal3: "#e9c46a",
+        pal4: "#f4a261",
+        pal5: "#e76f51",
+
+      },
+      fontFamily: {
+        sans: ["Neue Haas Grotesk Text Pro", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+
 }
